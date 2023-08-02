202bdb0d8ae69e27ba457746bdb9a5c87c67c16e
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2023-08-02 22:09:08 +0300

    add click event listener for create task btn to add new note

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,20 @@
 import { notes, setNotes } from './data/notes';
+import { Categories } from './data/categories';
+
+const createBtn = document.querySelector('.create-btn');
+
+createBtn.addEventListener('click', () => {
+	const note = {
+		id: notes.at(-1)?.id + 1 || 1,
+		title: 'New Note',
+		content: 'New Note Content',
+		date: new Intl.DateTimeFormat('en-US').format(new Date()),
+		category: Categories.Task.name,
+		dates: [],
+		archived: false,
+	};
+
+	setNotes([...notes, note]);
+	console.log(note);
+	console.log(notes);
+});
