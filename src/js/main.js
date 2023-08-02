b217b52f053d3ac3ceea130c2c1c1d073cd0cd61
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2023-08-02 23:44:35 +0300

    add modal and styles, change some styles for buttons

--- src/js/main.js
+++ src/js/main.js
@@ -2,19 +2,22 @@ import { notes, setNotes } from './data/notes';
 import { Categories } from './data/categories';
 
 const createBtn = document.querySelector('.create-btn');
+const modal = document.querySelector('.modal');
 
 createBtn.addEventListener('click', () => {
-	const note = {
-		id: notes.at(-1)?.id + 1 || 1,
-		title: 'New Note',
-		content: 'New Note Content',
-		date: new Intl.DateTimeFormat('en-US').format(new Date()),
-		category: Categories.Task.name,
-		dates: [],
-		archived: false,
-	};
+	modal.showModal();
 
-	setNotes([...notes, note]);
-	console.log(note);
-	console.log(notes);
+	// const note = {
+	// 	id: notes.at(-1)?.id + 1 || 1,
+	// 	title: 'New Note',
+	// 	content: 'New Note Content',
+	// 	date: new Intl.DateTimeFormat('en-US').format(new Date()),
+	// 	category: Categories.Task.name,
+	// 	dates: [],
+	// 	archived: false,
+	// };
+
+	// setNotes([...notes, note]);
+	// console.log(note);
+	// console.log(notes);
 });
