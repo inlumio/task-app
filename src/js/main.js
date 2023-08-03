2aaee510e4cc4db5cc33ae711c1f78d749e2a40f
inlumio <vladyslav.pjatkyvskyj@gmail.com>
2023-08-03 10:55:32 +0300

    add modal and first sketch of behavior

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,47 @@ import { Categories } from './data/categories';
 
 const createBtn = document.querySelector('.create-btn');
 const modal = document.querySelector('.modal');
+const modalForm = modal.querySelector('form');
+const submitModalBtn = modal.querySelector('button[type=submit]');
+const closeModalBtn = modal.querySelector('button[type=button]');
+const resetFormBtn = modal.querySelector('button[type=reset]');
+const nameField = document.querySelector('input[name="name"]');
+const contentField = document.querySelector('textarea[name="content"]');
+const categoryField = document.querySelector('select[name="category"]');
+
+modal.addEventListener('click', (e) => {
+	const dialogDimensions = modal.getBoundingClientRect();
+	if (
+		e.clientX < dialogDimensions.left ||
+		e.clientX > dialogDimensions.right ||
+		e.clientY < dialogDimensions.top ||
+		e.clientY > dialogDimensions.bottom
+	) {
+		modal.close();
+	}
+
+	if (e.target === submitModalBtn) {
+		e.preventDefault();
+		if (!nameField.value) {
+			nameField.setAttribute('aria-invalid', 'true');
+			return;
+		} else {
+			nameField.setAttribute('aria-invalid', 'false');
+		}
+		if (contentField.value.length < 10) {
+			contentField.setAttribute('aria-invalid', 'true');
+			return;
+		} else {
+			contentField.setAttribute('aria-invalid', 'false');
+		}
+
+		modal.close();
+		modalForm.reset();
+	}
+	if (e.target === closeModalBtn) {
+		modal.close();
+	}
+});
 
 createBtn.addEventListener('click', () => {
 	modal.showModal();
